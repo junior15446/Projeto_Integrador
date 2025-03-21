6c4f83883b4b196b9819580dcbdfc1a354259361
Medeiros096 <wellingtonmedeiros096@gmail.com>
2025-03-21 06:42:46 -0300

    iniciando CSS da page home

--- src/components/RegistrationForm/index.tsx
+++ src/components/RegistrationForm/index.tsx
@@ -1,25 +1,54 @@
-import { Link } from "react-router-dom"
+
+import { useState } from "react"
 
 function RegistrationForm() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [celular, setCelular] = useState('');
+  const [senha, setSenha] = useState('');
+  const [confirSenha, setConfirSenha] = useState('')
+
+  function handleSubmit() {
+    fetch('http://192.168.1.3:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        nome: nome,
+        email: 'email@email.com',
+        celular: '1234567890',
+        senha: 'senha',
+        confirSenha: 'confirSenha'
+      })
+    })
+  }
+
   return (
     <div className="registro">
-      <label htmlFor="">Nome</label>
-      <input type="text" name="nome" id="nome" placeholder="Digite seu nome completo" />
-      <label htmlFor="">Email</label>
+      <label htmlFor="nome">Nome</label>
+      <input 
+        type="text" 
+        name="nome" 
+        id="nome" 
+        placeholder="Digite seu nome completo"
+        value={nome}
+        onChange={(e) => setNome(e.target.value)}
+      />
+
+      <label htmlFor="email">Email</label>
       <input type="email" name="email" id="email" placeholder="Digite seu e-mail" />
-      <label htmlFor="">Celular</label>
+      <label htmlFor="celular">Celular</label>
       <input type="tel" name="celular" id="celular" placeholder="Informe seu numero de celular" />
-      <label htmlFor="">Senha</label>
+      <label htmlFor="senha">Senha</label>
       <input type="password" name="senha" id="senha" placeholder="Crie uma senha" />
-      <label htmlFor="">Confirme sua senha</label>
+      <label htmlFor="confirSenha">Confirme sua senha</label>
       <input type="password" name="confirSenha" id="confirSenha" placeholder="Confirme a senha" />
 
-      <Link to={'/perfil'} id="btn-registro" >
-        <input type="submit" value="Criar Conta" id="btn-registro" />
-      </Link>
+      <button onClick={handleSubmit}>Criar Conta</button>
     </div>
   )
 }
 
 export default RegistrationForm
-
+ 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,52 @@
+import React from 'react';
 import Card from "../components/Cards"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
+import "../styles/style.css"
 
-function Home() {
+const Home: React.FC = () => {
     return (
         <>
             <Header />
-            <div className="container">
-                <div className="boas-vindas">Bem-vindo ao Dengue-Off!
-                    Sua denúncia é fundamental para eliminar focos do mosquito e proteger a comunidade.
-                    É rápido, fácil e pode salvar vidas!
-                    Denunciar agora é um ato de cuidado e responsabilidade.
-                    Não espere! Denuncie já e faça a diferença!
+            <div className='container-home'>
+                <div className="boas-vindas">
+                    <h1>Bem-vindo ao Dengue-Off!</h1>
+                    <p>Sua denúncia é fundamental para eliminar focos do mosquito e proteger a comunidade.</p>
+                    <p>É rápido, fácil e pode salvar vidas!</p>
+                    <p>Denunciar agora é um ato de cuidado e responsabilidade.</p>
+                    <p>Não espere! Denuncie já e faça a diferença!</p>
                 </div>
                 <div className="container-cards">
-                    <Card titulo="Sintomas" img="https://th.bing.com/th/id/OIP.MNHMn1z1ADxjQVmfl_NPUQHaE8?rs=1&pid=ImgDetMain" imgAlt="dengue" descricao="A dengue geralmente causa febre alta, dores intensas no corpo (principalmente nas articulações e músculos), dor de cabeça forte, dor atrás dos olhos, náuseas, cansaço excessivo e erupções cutâneas."/>
-                    <Card titulo="Transmissão" img="https://th.bing.com/th/id/OIP.MNHMn1z1ADxjQVmfl_NPUQHaE8?rs=1&pid=ImgDetMain" imgAlt="dengue" descricao="A dengue é transmitida pela picada do mosquito Aedes aegypti infectado, que costuma se alimentar durante o dia, especialmente nas primeiras horas da manhã e ao entardecer."/>
-                    <Card titulo="Prevenção" img="https://th.bing.com/th/id/OIP.MNHMn1z1ADxjQVmfl_NPUQHaE8?rs=1&pid=ImgDetMain" imgAlt="dengue" descricao="A principal forma de prevenção é evitar o acúmulo de água parada, onde o mosquito pode depositar seus ovos. Isso inclui eliminar pneus, garrafas, potes e caixas d'água sem tampa, além de usar repelentes e roupas adequadas para se proteger da picada."/>
-                    <Card titulo="Tratamento" img="https://th.bing.com/th/id/OIP.MNHMn1z1ADxjQVmfl_NPUQHaE8?rs=1&pid=ImgDetMain" imgAlt="dengue" descricao="Não há tratamento específico para a dengue. O tratamento é focado em aliviar os sintomas, como o uso de analgésicos e hidratação adequada. Em casos graves, a pessoa pode necessitar de internação hospitalar."/>
-                    <Card titulo="Complicações" img="https://th.bing.com/th/id/OIP.MNHMn1z1ADxjQVmfl_NPUQHaE8?rs=1&pid=ImgDetMain" imgAlt="dengue" descricao="A dengue pode evoluir para formas graves, como a dengue hemorrágica ou síndrome do choque da dengue, que podem ser fatais se não tratadas rapidamente. Esses casos exigem cuidado médico intensivo."/>
+                    <Card 
+                        titulo="Sintomas" 
+                        img="https://th.bing.com/th/id/OIP.MNHMn1z1ADxjQVmfl_NPUQHaE8?rs=1&pid=ImgDetMain" 
+                        imgAlt="dengue" 
+                        descricao="A dengue geralmente causa febre alta, dores intensas no corpo (principalmente nas articulações e músculos), dor de cabeça forte, dor atrás dos olhos, náuseas, cansaço excessivo e erupções cutâneas."
+                    />
+                    <Card 
+                        titulo="Transmissão" 
+                        img="https://th.bing.com/th/id/OIP.MNHMn1z1ADxjQVmfl_NPUQHaE8?rs=1&pid=ImgDetMain" 
+                        imgAlt="dengue" 
+                        descricao="A dengue é transmitida pela picada do mosquito Aedes aegypti infectado, que costuma se alimentar durante o dia, especialmente nas primeiras horas da manhã e ao entardecer."
+                    />
+                    <Card 
+                        titulo="Prevenção" 
+                        img="https://th.bing.com/th/id/OIP.MNHMn1z1ADxjQVmfl_NPUQHaE8?rs=1&pid=ImgDetMain" 
+                        imgAlt="dengue" 
+                        descricao="A principal forma de prevenção é evitar o acúmulo de água parada, onde o mosquito pode depositar seus ovos. Isso inclui eliminar pneus, garrafas, potes e caixas d'água sem tampa, além de usar repelentes e roupas adequadas para se proteger da picada."
+                    />
+                    <Card 
+                        titulo="Tratamento" 
+                        img="https://th.bing.com/th/id/OIP.MNHMn1z1ADxjQVmfl_NPUQHaE8?rs=1&pid=ImgDetMain" 
+                        imgAlt="dengue" 
+                        descricao="Não há tratamento específico para a dengue. O tratamento é focado em aliviar os sintomas, como o uso de analgésicos e hidratação adequada. Em casos graves, a pessoa pode necessitar de internação hospitalar."
+                    />
+                    <Card 
+                        titulo="Complicações" 
+                        img="https://th.bing.com/th/id/OIP.MNHMn1z1ADxjQVmfl_NPUQHaE8?rs=1&pid=ImgDetMain" 
+                        imgAlt="dengue" 
+                        descricao="A dengue pode evoluir para formas graves, como a dengue hemorrágica ou síndrome do choque da dengue, que podem ser fatais se não tratadas rapidamente. Esses casos exigem cuidado médico intensivo."
+                    />
                 </div>
             </div>
             
